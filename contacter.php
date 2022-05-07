e603b0b2ef69676e0372ff78abe681090d459b41
NinoLombard <maxime.lombard@live.com.mx>
2022-05-07 17:30:02 +0200

    système de messagerie utilisant des API

--- contacter.php
+++ contacter.php
@@ -1,4 +1,11 @@
 <?php
+    require_once(__DIR__.'/vendor/autoload.php');
+    use \Mailjet\Resources;
+
+    define('API_USER','0b8a0215ff2ac5f0e49043ded14ce765');
+    define('API_LOGIN','edb2d13c74e777b367f6701d51de77fd');
+    $mj = new \Mailjet\Client('API_USER','API_LOGIN',true,['version' => 'v3.1']);
+
     if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
         $surname = htmlspecialchars($_POST['surname']);
         $firstname = htmlspecialchars($_POST['firstname']);
@@ -6,7 +13,28 @@
         $message = htmlspecialchars($_POST['message']);
 
         if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-            
+            $body = [
+                'Messages' => [
+                  [
+                    'From' => [
+                      'Email' => "maxime.lombard@live.com.mx",
+                      'Name' => "Maxime"
+                    ],
+                    'To' => [
+                      [
+                        'Email' => "maxime.lombard@live.com.mx",
+                        'Name' => "Maxime"
+                      ]
+                    ],
+                    'Subject' => "Demande de contact / Embauchement",
+                    'TextPart' => "$email, $message",
+                  ]
+                ]
+              ];
+              $response = $mj->post(Resources::$Email, ['body' => $body]);
+              $response->success();
+              echo "Email envoyé avec succès!";
+
         }else{
             echo "Cet email n'est pas valide";
         }
