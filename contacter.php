331264ef68bdfc2f67b4005bd8fdcb4dc0acbd57
NinoLombard <maxime.lombard@live.com.mx>
2022-05-07 16:30:19 +0200

    installed composer requirements for mailjet

---
+++ contacter.php
@@ -0,0 +1,18 @@
+<?php
+    if(!empty($_POST['surname']) && !empty($_POST['firstname']) && !empty($_POST['email']) && !empty($_POST['message'])){
+        $surname = htmlspecialchars($_POST['surname']);
+        $firstname = htmlspecialchars($_POST['firstname']);
+        $email = htmlspecialchars($_POST['email']);
+        $message = htmlspecialchars($_POST['message']);
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            
+        }else{
+            echo "Cet email n'est pas valide";
+        }
+
+
+    }else{
+        header('location:index.php');
+        die();
+    }
